a0881aa8a889e8d724d275f58f93fac40b02e38a
Yaspro <yasprogames1@gmil.com>
2025-07-23 16:37:50 +0300

    again

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -16,7 +16,7 @@ const Products = () => {
       originalPrice: "$99.99",
       image: "https://images.pexels.com/photos/3394650/pexels-photo-3394650.jpeg?auto=compress&cs=tinysrgb&w=400",
       category: "electronics",
-      delivery: 3,
+      delivery: 25,
       reviews: 128
     },
     {
